939cc39e66b5ec564d1cffeb33b50c8baa15dcc7
Abdullah Shaban <abdullah.shaaban91@gmail.com>
2016-05-20 16:03:37 +0300

    Fixed DB Connection
    Created the correct Database for the product catalogue

--- inc/database_connection.php
+++ inc/database_connection.php
@@ -3,7 +3,7 @@
 
 try{
 	//creates class that is used to connect to mysql
-	$db = new PDO('mysql:host=localhost;dbname=first site;charset=utf8mb4', 'abdullahshaaban', 'Whatyousaid@1');
+	$db = new PDO('mysql:host=localhost;dbname=product-catalogue;charset=utf8mb4', 'abdullahshaaban', 'Whatyousaid@1');
 	//This code will ensure that any error will be shown - and that execution pauses. The attributes have all been taken from the PDO Documentation
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 } 	//catches an exception in case the connection fails
