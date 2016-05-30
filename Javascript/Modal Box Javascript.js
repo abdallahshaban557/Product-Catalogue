ae60fedb61f7ac50f4106e3f2dbffe53dbbc38ae
abdullahshaaban <abdullah.shaaban91@gmail.com>
2016-05-30 18:52:28 +0300

    Upload javascript file
    Upload javascript file

--- Javascript/Modal Box Javascript.js
+++ Javascript/Modal Box Javascript.js
@@ -1,31 +1,14 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+var selectedValue;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+$('.modalAlert').on('click', function (event) {
+  var button = $(event.target) // Button that triggered the modal
+  var recipient = button.attr('value') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  $("#recipient-name").val(recipient)
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+})
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function test(event)
-{
-    alert(event.target.getAttribute("value"))
-    
-}
+//functon fillReciepant(event)
