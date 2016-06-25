7dc4becc60f09b9cb915bea3e0638c8521c83828
Abdullah Shaban <abdullah.shaaban91@gmail.com>
2016-06-25 18:18:49 +0300

    updated modal box
    Updated modal box

--- js/Modal Box Javascript.js
+++ js/Modal Box Javascript.js
@@ -17,20 +17,36 @@ $('.modalAlert').on('click', function () {
   //created element which will be inserted into the Modal
   var insertedHTML;
   
-  //loop used to traverse the json response of the URL
+  //loop used to traverse the json response of the URL - JSON parse is used on the data in order to ensure that the object is JSON to be looped correctly 
   $.each(JSON.parse(response), function(index, unit){
-      
+    
       insertedHTML += "<tr class = 'ajax-input'>"
       insertedHTML += "<td>"+unit.Total_Units+"</td>";
       insertedHTML += "<td>"+unit.DA_ID+"</td>";
+      insertedHTML += "<td><a class = 'glyphicon glyphicon-edit modal-edit-button' data-value = '" + unit.Total_Units_ID + "'></td>";
+      insertedHTML += "<td><a class ='glyphicon glyphicon-remove-circle modal-delete-button' data-value = '" + unit.Total_Units_ID + "'/></td>";
       insertedHTML += "</td>";
       
   });//end .each loop
-  
   //add the HTML needed to the modal table
   $("#modal-table").append(insertedHTML);
   
-  });//end ajax get request
+  });//end ajax get request  
   
-  
-});//end on click event
+});//end on click event for the main packages page units view
+
+
+//need to use this format for the button click because of the AJAX Call
+$(document).on("click", ".modal-edit-button", function() {
+
+    alert($(this).attr("data-value"));
+    
+});//End on click event for the modal edit button
+
+
+
+$(document).on("click", ".modal-delete-button", function() {
+
+    alert($(this).attr("data-value"));
+    
+});//End on click event for the modal edit button
